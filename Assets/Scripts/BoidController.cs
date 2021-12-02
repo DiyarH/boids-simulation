4fc2730bac90d40478d7333e03b93e088feb3829
Diyar Hamedi <diyarhamedi81@gmail.com>
2021-12-03 00:37:19 +0330

    Add boid script

---
+++ Assets/Scripts/BoidController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidController : MonoBehaviour
+{
+    public float minInitialSpeed;
+    public float maxInitialSpeed;
+    public Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rigidbody.rotation = Random.Range(-180, 180);
+        transform.Rotate(0, 0, Random.Range(-180, 180));
+        rigidbody.velocity = (Vector2)transform.up * Random.Range(minInitialSpeed, maxInitialSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
